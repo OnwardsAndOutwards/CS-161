64b618540e4b9211d9ccca5c33c9d2f1f8878ecc
Daniel Thomas <tfriesdan@gmail.com>
2019-02-19 15:15:19 -0800

    Started Lab 7

--- Lab7/DanielFriesLab7_1/main.cpp
+++ Lab7/DanielFriesLab7_1/main.cpp
@@ -7,15 +7,46 @@
 
 using namespace std;
 
+char computerMove, userMove;
+
+char displayInstructions();
+char getMove();
+int getWinner(char computerMove, char userMove);
+bool playAgain();
+
 int main()
 {
     cout << "Welcome to Rock, Paper, Scissors!\n\n" << endl;
-    cout << "     Rock beats Scissors" << endl;
-    cout << "       Paper beats Rock" << endl;
-    cout << "     Scissors beats Paper" << endl;
+    cout << displayInstructions() << endl;
+    cout << getMove() << endl;
+
 
     bool playing = true;
 
 
     return 0;
 }
+char displayInstructions()
+{
+    cout << "     Rock beats Scissors" << endl;
+    cout << "       Paper beats Rock" << endl;
+    cout << "     Scissors beats Paper" << endl;
+}
+char getMove()
+{
+    cout << "What is your move?\n\n R = Rock // S = Scissors // P = Paper" << endl;
+    cin >> userMove;
+    cout << userMove;
+
+    while ( userMove != 'R' or userMove != 'P' or userMove != 'S' )
+    {
+        cout << "Invalid input!  Try again:  " << endl;
+        cin >> userMove;
+
+    }
+}
+int getWinner(char computerMove, char userMove)
+{
+
+}
+//bool playAgain()
